ad9ded93fb0992fb15ce4661c5e4eb779395d382
Abdulrahman.saad <abdulrahman.saad@squadio.com>
2024-03-17 22:34:02 +0200

    refactor the functiion

--- index.js
+++ index.js
@@ -1,57 +1,25 @@
-// function insertionSort(arr) {
-//     debugger;
-//       const n = arr.length;
-//       for (let i = 1; i < n; i++) {
-//           let current = arr[i];
-//           let j = i - 1;
-///               arr[j + 1] = arr[j];
-//               j--;
-//           }
-//           arr[j + 1] = current;
-//       }
-//       return arr;
-//   }
 
-
-// 1- n = arr.length
-// 2- for i = 1; i < n ; i++
-// 3- while backowrd j => 0 && arr[j] > arr[j + 1];
-
-
-
- function insertionSort(arr) {
-    debugger
+function insertionSort(arr) {
     var n = arr.length
-    var i = 1;
-    j = (i - 1);
-    for (var i = 1; i < n; i++) {
-      var  current = arr[i];
-      j = (i - 1);
-    //   var  j =( i - 1);
+    for (let i = 1; i < n; i++) {
+        let current = arr[i];
+        let j = i - 1;
 
-        while(j >= 0 && (arr[j] > current)) {
-           arr[j + 1] = arr[j];
-           j--
+        while (j >= 0 && (arr[j] > current)) {
+            arr[j + 1] = arr[j];
+            j--
         }
         arr[j + 1] = current;
     }
-    return arr
+    return arr;
 }
 
 
 
 
 
-
-  
-  // Example usage:
-  const array = [5, 2, 4, 6, 1, 3];
-  console.log("Original array:", array);
-  const sortedArray = insertionSort(array.slice()); // Passing a copy of the array to keep the original intact
-  console.log("Sorted array:", sortedArray);
-  
-
-
-// use cases 
-// 1- when j == -1 so break wwhenh !( j >= 0  )
-// 2- .. arr[j] > current so break when !( arr[j] > current )
+// Example usage:
+const array = [5, 2, 4, 6, 1, 3];
+console.log("Original array:", array);
+const sortedArray = insertionSort(array.slice()); // Passing a copy of the array to keep the original intact
+console.log("Sorted array:", sortedArray);
