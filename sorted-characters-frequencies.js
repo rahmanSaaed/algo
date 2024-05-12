c4ab534d6901e46cbaf1cc94bddbd529ff2c6022
Abdulrahman.saad <abdulrahman.saad@squadio.com>
2024-05-12 19:00:45 +0300

    sort

--- sorted-characters-frequencies.js
+++ sorted-characters-frequencies.js
@@ -18,24 +18,94 @@
     // else add it to the object and assign 1 to the value 
 
 
+    /// sortHsh
+      //  init varArray
+      //  loop over object
+      //  push to the array new array contain first el is charCode and the second the freq
+
+
+    
+  function sortHsh(freq) {
+    let freqArray = [];
+    for (let k of Object.keys(freq)) {
+      freqArray.push([k.charCodeAt(0), freq[k]]);
+    }
+
+
+    sort(freqArray, 0, freqArray.length - 1);
+  }
+
+  function sort(array, start, end) {
+    
+    if( start >= end) {
+      return;
+    } 
+
+    let midPoint = Math.floor((start + end)/ 2);
+
+    sort(array, start, midPoint);
+    sort(array, midPoint + 1, end);
+    
+    merge(array, start, midPoint, end);
+    console.log("merge", array);
+  }
+
+  function merge(array, start, midPoint, end) {
+    let leftArray, rightArray;
+
+    leftArray = array.slice(start, midPoint + 1);
+    rightArray = array.slice(midPoint +  1, end + 1);
+
+    let i, j, k;
+    i = 0;
+    j = 0;
+    k = start;
+
+    while (i < leftArray.length && rightArray.length > j) {
+
+      if (leftArray[i][1] <= rightArray[j][1]) {
+        array[k] = leftArray[i];
+        i++;
+        k++
+      } else {
+        array[k] = rightArray[j];
+        j++;
+        k++
+      }
+
+    } 
+
+
+
+    while (leftArray.length > i) {
+      array[k] = leftArray[i];
+      i++;
+      k++;
+    }
+
+    while (rightArray.length > j) {
+      array[k] = rightArray[j];
+      j++;
+      k++;
+    }
+
+
+  }
+
+
   function  AnyCodeMethod(message) {
     let reqObj = {};
-
     for (let i = 0; i < message.length; i++) {
         if ( reqObj[message[i]] ) {
             reqObj[message[i]]++
         } else {
             reqObj[message[i]] = 1;
         }
-
     }
-
-    console.log("AnyCodeMethod", reqObj);
-
+    sortHsh(reqObj)
     }
 
  function charFreq(message) {
-    // debugger
     const chars = new Array(127).fill(0)
 
     for(let i = 0; i < message.length; i++) {
